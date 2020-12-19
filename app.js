c396a55ec392860982402a61bd10fbe982da620f
Ayush Kushwaha <58135144+ayushkx@users.noreply.github.com>
2020-12-19 23:00:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express=require('express');
+const app=express();
+const bodyparser=require('body-parser');
+const request=require('request');
+ const https=require('https');
+ app.use(bodyparser.urlencoded({extended:true}));
+
+ app.use(express.static("public"));
+
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    
+   const firstname= req.body.fname;
+   const lastname= req.body.lname;
+    const email=req.body.email;
+   const data={
+   	members:[
+   	{
+   		email_address:email,
+   		status:"subscribed",
+   		merge_fields:{
+   			FNAME:firstname,
+   			LNAME: lastname,
+   		}
+   	}
+   	]
+   };
+
+    const jsondata=JSON.stringify(data);
+
+    const url="https://us10.api.mailchimp.com/3.0/lists/7687ab690d";
+    const options={
+    	method:"POST",
+    	auth:"ayush:c0c274d0d0a965fbb373e712f55cea4a-us10"
+    }
+    const request=https.request(url,options,function(response){
+        if (response.statusCode===200){
+          res.sendFile(__dirname+"/success.html");
+        }
+       else
+       {
+        res.sendFile(__dirname+"/failure.html");
+       }
+    	response.on("data",function(data){
+    		console.log(JSON.parse(data));
+    	});
+    });
+
+    request.write(jsondata);
+    request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("server started at 3000");
+});
+// 
+
